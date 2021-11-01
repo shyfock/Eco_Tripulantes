c140349a7fcf49a290b5e85f44ce56a86a101337
shyfock <agvanegas@gmail.com>
2021-10-31 19:06:55 -0500

    server + Mongo DB connection

---
+++ server/config/database.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const host = "localhost";
+const port = "27017";
+const db = "hr";
+
+exports.mongoConnect = () => {
+    const mongoStringConnection = `mongodb://${host}:${port}/${db}`;
+
+    mongoose.connect(mongoStringConnection);
+    mongoose.Promise = global.Promise;
+    const dbConnection = mongoose.connection;
+    dbConnection.on("error", console.error.bind(console, "MongoDB connection error"))
+    dbConnection.once("open", () => {
+        console.log("DB - OK! in port: " + `${host}:${port}`);
+    })
+}
