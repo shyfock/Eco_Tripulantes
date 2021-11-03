b7580b9bcd5555ec4c93c5d1bfbdd0fea31d6224
shyfock <agvanegas@gmail.com>
2021-11-03 15:30:16 -0500

    Borramos archivos *.jade, index.js y users.js

--- server/app.js
+++ server/app.js
@@ -5,14 +5,10 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var database = require('./config/database')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -24,8 +20,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 database.mongoConnect();
 
 //Router
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+
+// Mensaje a página del navegador
+app.use((req, res) => {
+  res.send({code: req.headers.connection, msg: "Estamos en construcción, code: " + `${res.statusCode}`})
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -40,7 +40,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json('error');
 });
 
 module.exports = app;
