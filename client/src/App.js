b42c0c4912616bc50134052b7ec4b94919a32735
shyfock <agvanegas@gmail.com>
2021-11-09 15:05:26 -0500

    Modificamos /client/App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -42,30 +41,6 @@ class App extends Component {
       </div>
     );
   }
-=======
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
->>>>>>> 7d55bc5 (Adjunta .gitignore)
 }
 
 export default App;
