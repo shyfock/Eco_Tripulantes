95c11cb697f8b9721f54dcaee5c737894d5627b4
shyfock <agvanegas@gmail.com>
2021-11-09 14:30:27 -0500

    Rebase sobre main

--- server/config/database.js
+++ server/config/database.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
 const host = "localhost";
 const port = "27017";
+<<<<<<< HEAD
 const db = "ecoDB";
+=======
+const db = "hr";
+>>>>>>> 97596be (Rebase client sobre main)
 
 exports.mongoConnect = () => {
     const mongoStringConnection = `mongodb://${host}:${port}/${db}`;
@@ -10,7 +14,12 @@ exports.mongoConnect = () => {
     mongoose.Promise = global.Promise;
     const dbConnection = mongoose.connection;
     dbConnection.on("error", console.error.bind(console, "MongoDB connection error"))
+<<<<<<< HEAD
     dbConnection.once("connected", () => {
         console.log("DB - OK! on port: http://" + `${host}:${port}`);
+=======
+    dbConnection.once("open", () => {
+        console.log("DB - OK! in port: " + `${host}:${port}`);
+>>>>>>> 97596be (Rebase client sobre main)
     })
 }
