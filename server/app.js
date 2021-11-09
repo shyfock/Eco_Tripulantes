6b04b9159786c01bb840ae15fad8131a1572f13a
shyfock <agvanegas@gmail.com>
2021-11-09 15:39:06 -0500

    Actualización rama main

--- server/app.js
+++ server/app.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-<<<<<<< HEAD
 var cors = require('cors');
 var database = require('./config/database');
 var auth = require('./auth/main.auth');
@@ -12,25 +11,13 @@ var testSERVERRouter = require('./routes/testSERVER');
 var publicationsRouter = require('./routes/publication.router');
 var usersRouter = require('./routes/usuario.router');
 
-=======
-var database = require('./config/database')
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
->>>>>>> 97596be (Rebase client sobre main)
 
 var app = express();
 
 // view engine setup
-<<<<<<< HEAD
 // app.set('views', path.join(__dirname, 'views'));
 
 app.use(cors());
-=======
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
->>>>>>> 97596be (Rebase client sobre main)
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -41,7 +28,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 database.mongoConnect();
 
 //Router
-<<<<<<< HEAD
 
 app.use('/users', usersRouter);
 
@@ -57,11 +43,6 @@ app.use('/publications', publicationsRouter);
 //   res.send({code: req.headers.connection, msg: "Estamos en construcción, code: " + `${res.statusCode}`})
 // })
 
-=======
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
->>>>>>> 97596be (Rebase client sobre main)
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -75,11 +56,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-<<<<<<< HEAD
   res.json('error');
-=======
-  res.render('error');
->>>>>>> 97596be (Rebase client sobre main)
 });
 
 module.exports = app;
--- server/config/database.js
+++ server/config/database.js
@@ -1,11 +1,7 @@
 const mongoose = require("mongoose");
 const host = "localhost";
 const port = "27017";
-<<<<<<< HEAD
 const db = "ecoDB";
-=======
-const db = "hr";
->>>>>>> 97596be (Rebase client sobre main)
 
 exports.mongoConnect = () => {
     const mongoStringConnection = `mongodb://${host}:${port}/${db}`;
@@ -14,12 +10,7 @@ exports.mongoConnect = () => {
     mongoose.Promise = global.Promise;
     const dbConnection = mongoose.connection;
     dbConnection.on("error", console.error.bind(console, "MongoDB connection error"))
-<<<<<<< HEAD
     dbConnection.once("connected", () => {
         console.log("DB - OK! on port: http://" + `${host}:${port}`);
-=======
-    dbConnection.once("open", () => {
-        console.log("DB - OK! in port: " + `${host}:${port}`);
->>>>>>> 97596be (Rebase client sobre main)
     })
 }
